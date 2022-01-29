75bb67785527f561cabbf06c48671c74dafe5e31
NatnaelM77 <natnaelmehari77@gmail.com>
2022-01-29 17:35:13 +0300

    update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,37 @@
 #!/usr/bin/python3
+
+"""
+module BaseModel
+"""
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """Class BaseModel"""
+
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        """Returns printable string representation"""
+        str = f'[self.__class__.__name__] ({self.id}) {self.__dict__}'
+        return str
+
+    def save(self):
+        """Updates the attribute updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of __dict__ of
+        the instance."""
+        dct = self.__dict__.copy()
+        dct.update({
+            '__class__': self.__class__.__name__,
+            'created_at': self.created_at.isoformat(),
+            'updated_at': self.updated_at.isoformat()
+        })
+        return dct
