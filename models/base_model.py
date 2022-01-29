3a2de6ab64af7dd8083e0296774e0596078190f1
NatnaelM77 <natnaelmehari77@gmail.com>
2022-01-29 20:16:01 +0300

    update file_storage.py base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,7 @@ module BaseModel
 """
 
 import uuid
-from models import storage
+import models
 from datetime import datetime
 
 class BaseModel:
@@ -24,8 +24,8 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            storage.new(self)
-            storage.save()
+            models.storage.new(self)
+            models.storage.save()
 
     def __str__(self):
         """Returns printable string representation"""
@@ -36,7 +36,7 @@ class BaseModel:
     def save(self):
         """Updates the attribute updated_at with the current datetime"""
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of __dict__ of
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,16 +6,21 @@ module FileStorage
 
 import os
 import json
-
+from models.base_model import BaseModel
 
 class FileStorage:
-    """Class FileStorage"""
+    """Class FileStorage
+       Attributes:
+           __file_path (str): The name of the file
+           __objects (dict): A dictionary of instantiated objects
+    """
+
     __file_path = 'file.json'
     __objects = {}
 
     def all(self):
         """Returns the dictionary __objects"""
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id"""
@@ -23,16 +28,26 @@ class FileStorage:
             cls_name = obj.__class__.__name__
             obj_id = obj.id
             self.__objects.update({
-                obj: f'{cls_name}.{obj_id}'
+                f'{cls_name}.{obj_id}': obj
             })
 
     def save(self):
         """Serializes __objects to the JSON file"""
+        json_objects = {}
+        for key in self.__objects.keys():
+            json_objects.update({
+                key: self.__objects[key].to_dict()
+            })
         with open(self.__file_path, 'w') as f:
-            pass
+            json.dump(json_objects, f)
 
     def reload(self):
         """Deserialize the JSON file to __objects"""
         if os.path.exists(self.__file_path):
-            with open(self.__file_path) as file:
-                pass
+            with open(self.__file_path, 'r') as file:
+                my_obj = {}
+                dct = json.load(file)
+                for key, value in dct.items():
+                    cls_name = key.split('.')[0]
+                    my_obj = eval(cls_name)(**value)
+                    self.new(my_obj)
